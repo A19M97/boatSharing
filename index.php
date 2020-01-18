bd16dc1ac6281be33ec4ce13a4ec0f43e4b20a95
Andrea Mugnai <andrea.mugnai97@gmail.com>
2020-01-18 18:56:02 +0100

    navbar added

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
     define('__ROOT__', dirname(dirname(__FILE__)));
     require_once(__ROOT__."/boatSharing/php/header.php");
     require_once(__ROOT__."/boatSharing/php/footer.php");
+    require_once(__ROOT__."/boatSharing/php/navbar.php");
     the_header("Homepage");
+    echo "<body>";
+    the_navbar();
+    echo "</body>";
     the_footer();
 ?>
