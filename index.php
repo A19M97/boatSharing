689100a506e0879b6aa53dc2356e6cc62ae719d1
Andrea Mugnai <andrea.mugnai97@gmail.com>
2020-01-19 20:08:00 +0100

    general css added & hompage improved

--- index.php
+++ index.php
@@ -4,10 +4,22 @@
     require_once(__ROOT__."/boatSharing/php/footer.php");
     require_once(__ROOT__."/boatSharing/php/navbar.php");
     the_header("Homepage");
-    echo "<body>";
-    echo "<div class=\"container-fluid\">";
-    //the_navbar();
-    echo "</div>";
-    echo "</body>";
+    ?>
+    <body>
+        <div class="container-fluid homepage-container">
+            <div class="row">
+                <div id="hp-login-button" class="col-md-12 text-right align-middle">
+                    <a href="/boatSharing/login" class="align-middle hp-a-login">Login</a>
+                </div>
+            </div>      
+            <div class="row">
+                <div class="col-md-4 offset-md-4 text-center">
+                    <h1 id="hp-main-title" class="primary-color">BoatSharing</h1>
+                    <h2 id="hp-subtitle" class="secondary-color">Una barca a portata di mano</h1>
+                </div>
+            </div>
+        </div>
+    </body>
+    <?php
     the_footer();
 ?>
--- php/header.php
+++ php/header.php
@@ -5,8 +5,9 @@
         <html lang=\"it\">
             <head>
                 <title>BoatSharing - $title</title>
+                <link rel=\"stylesheet\" href=\"/boatSharing/css/general-style.css\">
                 <link rel=\"stylesheet\" href=\"/boatSharing/css/bootstrap/bootstrap.min.css\">
-                <link rel=\"stylesheet\" href=\"/boatSharing/css/navbar-style.css\">
+                <link rel=\"stylesheet\" href=\"/boatSharing/css/homepage-style.css\">
                 <script src=\"/boatSharing/js/bootstrap/bootstrap.min.js\"></script>
             </head>";
     }
